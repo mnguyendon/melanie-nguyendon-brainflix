eec24b4742cd18dfad6c1650e85ab91c71c5287d
Melanie Nguyen Don <mnguyendon@gmail.com>
2024-01-18 00:21:51 -0800

    added videoInfo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Video from "./components/Video/Video";
+import VideoInfo from "./components/VideoInfo/VideoInfo";
 
 function App() {
   return (
     <>
       <Header />
       <Video />
+      <VideoInfo />
     </>
   );
 }
---
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -0,0 +1,30 @@
+import "./VideoInfo.scss";
+
+function VideoInfo() {
+  return (
+    <section className="videoInfo">
+      <h1 className="videoInfo__title">BMX Rampage: 2021 Highlights</h1>
+      <div className="videoInfo__specifics">
+        <div className="videoInfo__specifics-static">
+          <h2 className="videoInfo__author videoInfo__item">By Red Cow</h2>
+          <p className="videoInfo__date videoInfo__item">07/11/2021</p>
+        </div>
+        <div className="videoInfo__specifics-dynamic">
+          <p className="videoInfo__views videoInfo__item">views</p>
+          <p className="videoInfo__likes videoInfo__item">likes</p>
+        </div>
+      </div>
+      <p className="videoInfo__description">
+        On a gusty day in Southern Utah, a group of 25 daring mountain bikers
+        blew the doors off what is possible on two wheels, unleashing some of
+        the biggest moments the sport has ever seen. While mother nature only
+        allowed for one full run before the conditions made it impossible to
+        ride, that was all that was needed for event veteran Kyle Strait, who
+        won the event for the second time -- eight years after his first Red Cow
+        Rampage title
+      </p>
+    </section>
+  );
+}
+
+export default VideoInfo;
